708bc5559a6c3a2de8122b64835ae15a73aab809
--replace-all <docziadrianmark@turr.hu>
2025-01-17 10:29:47 +0100

    Fájlbeolvasás elkezdése

---
+++ Receptek/ConsoleApp1/Adatbazis.cs
@@ -0,0 +1,86 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Adatbazis
+    {
+        private static string connectionString = "Server=localhost;Database=receptek_12a;User id=root;password=;";
+        private static MySqlConnection connection = new MySqlConnection(connectionString);
+
+        public static void KapcsolodasAdatbazishoz()
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Kapcsolat sikeresen megnyitva.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba a kapcsolat megnyitásakor: " + ex.Message);
+            }
+        }
+
+        public static List<Dictionary<string, object>> TableSelect(string tableName)
+        {
+            var results = new List<Dictionary<string, object>>();
+            var osszesRecept = new List<string>();
+
+            try
+            {
+                using (MySqlCommand selectCommand = new MySqlCommand($"SELECT * FROM {tableName};", connection))
+                {
+                    using (MySqlDataReader dataReader = selectCommand.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            var row = new Dictionary<string, object>();
+                            for (int i = 0; i < dataReader.FieldCount; i++)
+                            {
+                                string columnName = dataReader.GetName(i);
+                                object columnValue = dataReader.IsDBNull(i) ? null : dataReader.GetValue(i);
+                                row[columnName] = columnValue;
+
+                                if (columnValue != null)
+                                {
+                                    osszesRecept.Add(columnValue.ToString());
+                                }
+                            }
+                            results.Add(row);
+                        }
+                    }
+                }
+
+                int szamlalo = 0;
+                string sor = "";
+                foreach (string recept in osszesRecept)
+                {
+                    szamlalo++;
+                    sor += recept + ";";
+                    if (szamlalo == 9)
+                    {
+                        Receptek.ReceptBeolvasas(sor);
+                        sor = "";
+                        szamlalo = 0;
+                    }
+                }
+
+                
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Hiba a kapcsolódás vagy a lekérdezés során: " + error.Message);
+            }
+            finally
+            {
+                if (connection != null && connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                    Console.WriteLine("Kapcsolat sikeresen lezárva.");
+                }
+            }
+            return results;
+        }
+    }
+}
---
+++ Receptek/ConsoleApp1/Receptek.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Receptek
+    {
+        private int id;
+        private string receptNev;
+        private string hozzavalok;
+        private string leiras;
+        private int elokeszitesiIdo;
+        private int fozesiIdo;
+        private int osszesIdo;
+        private int keszito_id;
+        private int forras_id;
+
+        
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                if(this.id >= 0 && this.id <= 9999)
+                {
+                    this.id = value;
+                }
+                
+            }
+        }
+
+        public string ReceptNev
+        {
+            get
+            {
+                return this.receptNev;
+            }
+            set
+            {
+                if (this.receptNev != null && this.receptNev.Length > 1)
+                {
+                    this.receptNev = value;
+                }
+
+            }
+        }
+
+        public string Hozzavalok
+        {
+            get
+            {
+                return this.hozzavalok;
+            }
+            set
+            {
+                if (this.hozzavalok != null && this.hozzavalok.Length > 1)
+                {
+                    this.hozzavalok = value;
+                }
+
+            }
+        }
+        public string Leiras
+        {
+            get
+            {
+                return this.leiras;
+            }
+            set
+            {
+                if (this.leiras != null && this.leiras.Length > 1)
+                {
+                    this.leiras = value;
+                }
+
+            }
+        }
+        public int ElokeszitesiIdo
+        {
+            get
+            {
+                return this.elokeszitesiIdo;
+            }
+            set
+            {
+                if (this.elokeszitesiIdo >= 0 && this.elokeszitesiIdo <= 9999)
+                {
+                    this.elokeszitesiIdo = value;
+                }
+
+            }
+        }
+        public int FozesiIdo
+        {
+            get
+            {
+                return this.fozesiIdo;
+            }
+            set
+            {
+                if (this.fozesiIdo >= 0 && this.fozesiIdo <= 9999)
+                {
+                    this.fozesiIdo = value;
+                }
+
+            }
+        }
+        public int OsszesIdo
+        {
+            get
+            {
+                return this.osszesIdo;
+            }
+            set
+            {
+                if (this.osszesIdo >= 0 && this.osszesIdo <= 9999)
+                {
+                    this.osszesIdo = value;
+                }
+
+            }
+        }
+        public int Keszito_id
+        {
+            get
+            {
+                return this.keszito_id;
+            }
+            set
+            {
+                if (this.keszito_id >= 0 && this.keszito_id <= 9999)
+                {
+                    this.keszito_id = value;
+                }
+
+            }
+        }
+        public int Forras_id
+        {
+            get
+            {
+                return this.forras_id;
+            }
+            set
+            {
+                if (this.forras_id >= 0 && this.forras_id <= 9999)
+                {
+                    this.forras_id = value;
+                }
+
+            }
+        }
+
+
+        public Receptek(int id, string receptNev, string hozzavalok, string leiras, int elokeszitesiIdo, int fozesiIdo, int osszesIdo, int keszito_id, int forras_id)
+        {
+            Id = id;
+            ReceptNev = receptNev;
+            Hozzavalok = hozzavalok;
+            Leiras = leiras;
+            ElokeszitesiIdo = elokeszitesiIdo;
+            FozesiIdo = fozesiIdo;
+            OsszesIdo = osszesIdo;
+            Keszito_id = keszito_id;
+            Forras_id = forras_id;
+        }
+
+        public static void ReceptBeolvasas(string sor)
+        {
+            var id = sor.Split(';')[0];
+            Console.WriteLine(id);
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Recept neve: {receptNev}";
+        }
+
+
+    }
+}
