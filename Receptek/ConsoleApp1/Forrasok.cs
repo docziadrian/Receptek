81fe9bfda717c96e3ed5f60c41b001b5ce8cbb28
MrErik402 <bujakierikattila@turr.hu>
2025-01-22 10:17:13 +0100

    Készítők és Források osztály elkészítése.

---
+++ Receptek/ConsoleApp1/Forrasok.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Forrasok
+    {
+        private int Id;
+        private string Nev;
+
+        public int id { get => Id; set => Id = value; }
+        public string nev { get => Nev; set => Nev = value; }
+
+        public Forrasok(int id, string nev)
+        {
+            this.id = id;
+            this.nev = nev;
+        }
+    }
+}
---
+++ Receptek/ConsoleApp1/Keszitok.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Keszitok
+    {
+        private int Id;
+        private string Nev;
+        private string Lakcim;
+        private int Eletkor;
+
+        public int id { get => Id; set => Id = value; }
+        public string nev { get => Nev; set => Nev = value; }
+        public string lakcim { get => Lakcim; set => Lakcim = value; }
+        public int eletkor { get => Eletkor; set => Eletkor = value; }
+
+        public Keszitok(int id, string nev, string lakcim, int eletkor)
+        {
+            this.id = id;
+            this.nev = nev;
+            this.lakcim = lakcim;
+            this.eletkor = eletkor;
+        }
+    }
+
+}
